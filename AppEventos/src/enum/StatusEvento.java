b6802cbd5f03828a58fa42fc04a25602d9927d5c
M4T3U5 <b10h4z4rd01@gmail.com>
2017-07-22 00:31:02 -0300

    added enum types

---
+++ AppEventos/src/enum/StatusEvento.java
@@ -0,0 +1,12 @@
+package enum;
+
+public enum StatusEvento
+{
+	NOVO(1), INSCRIÇÕES_ABERTAS(2), EM_ANDAMENTO(3), FINALIZADO(4);
+
+	public int StatusEvento;
+	StatusEvento(int valor){
+		StatusEvento = valor;		
+	}
+
+}
